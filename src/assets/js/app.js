17b38a116e0167ec3480854a6a3b3976c784595a
Thomas Johnston <tpickles211@gmail.com>
2015-12-20 20:29:00 -0800

    Adding mouse movement listeners

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,26 @@
+'use strict';
+
 console.log('hi');
+
+const $ = require('jquery');
+
+let $container = $('#card-container');
+
+function mousemove(event) {
+  console.log(event.clientX);
+}
+
+function mousedown(event) {
+  $container.on('mousemove', mousemove);
+  $container.on('mouseup', mouseup);
+  $container.off('mousedown', mousedown);
+}
+
+
+function mouseup(event) {
+  $container.off('mousemove', mousemove);
+  $container.off('mouseup', mouseup);
+  $container.on('mousedown', mousedown);
+}
+
+$container.on('mousedown', mousedown);
