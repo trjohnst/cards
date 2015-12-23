7935b322dd0bfe9e7e2875f9a631b36c292b5a5b
trjohnst <tpickles211@gmail.com>
2015-12-22 16:39:43 -0800

    Adding import support to postcss build

--- tools/build/handlers/css.js
+++ tools/build/handlers/css.js
@@ -6,21 +6,23 @@ const postcss = require('postcss');
 
 function build() {
   exec('mkdir -p dist/assets/css', (error, stdout, stderr) => {
-    fs.readFile('src/assets/css/styles.css', (err, css) => {
+
+    fs.readFile('src/assets/css/index.css', (err, css) => {
       if (err) {
         console.log('error reading file');
         return;
       }
 
       postcss([
+          require('postcss-import'),
           require('autoprefixer'),
           require('postcss-simple-vars'),
           require('postcss-nested'),
           require('cssnano')
         ])
+        .use(require('postcss-import'))
         .process(css, {
-          from: 'src/assets/css/styles.css',
-          to: 'dist/assets/css/styles.css'
+          from: 'src/assets/css/index.css'
         })
         .then((result) => {
           fs.writeFileSync('dist/assets/css/styles.css', result.css);
